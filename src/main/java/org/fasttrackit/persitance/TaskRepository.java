f15535b143a3abf9e2eaab716e5ff0dfab715e62
Flaviu Ratiu <ratiu.flaviu@gmail.com>
2020-05-11 20:32:47 +0300

    implemented CRUD methods in task repository

--- src/main/java/org/fasttrackit/persitance/TaskRepository.java
+++ src/main/java/org/fasttrackit/persitance/TaskRepository.java
@@ -1,13 +1,13 @@
 package org.fasttrackit.persitance;
 
+import org.fasttrackit.domain.Task;
 import org.fasttrackit.transfer.CreateTaskRequest;
+import org.fasttrackit.transfer.UpdateTaskRequest;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.time.LocalDate;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TaskRepository {
 
@@ -25,4 +25,57 @@ public class TaskRepository {
             preparedStatement.executeUpdate();
         }
     }
+
+    public void updateTask(long id, UpdateTaskRequest request) throws SQLException, IOException, ClassNotFoundException {
+        String sql = "UPDATE task SET done=? WHERE id=?";
+
+        try (Connection connection = DatabaseConfiguration.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+
+            preparedStatement.setBoolean(1, request.isDone());
+            preparedStatement.setLong(2, id);
+
+            preparedStatement.executeUpdate();
+        }
+    }
+
+    public void deleteTask(long id) throws SQLException, IOException, ClassNotFoundException {
+        String sql = "DELETE FROM task WHERE id = ?";
+
+        try (Connection connection = DatabaseConfiguration.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+
+            preparedStatement.setLong(1, id);
+
+            preparedStatement.executeUpdate();
+        }
+    }
+
+    public List<Task> getTasks() throws SQLException, IOException, ClassNotFoundException {
+        String sql = "SELECT id, description, deadline, done FROM task WHERE done = ?";
+
+        List<Task> tasks = new ArrayList<>();
+
+        try (Connection connection = DatabaseConfiguration.getConnection();
+             Statement statement = connection.createStatement()) {
+
+            ResultSet resultSet = statement.executeQuery(sql);
+
+            while (resultSet.next()) {
+                Task task = new Task();
+                task.setId(resultSet.getLong("id"));
+                task.setDescription(resultSet.getString("description"));
+
+                Date deadlineAsSqlDate = resultSet.getDate("deadline");
+                task.setDeadline(deadlineAsSqlDate.toLocalDate());
+
+                task.setDone(resultSet.getBoolean("done"));
+
+                tasks.add(task);
+            }
+        }
+
+        return tasks;
+    }
+
 }
